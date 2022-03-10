d02433c7be9209f9d3f29b619f655e535e3ba0fc
LeaGrmx <lea.grumiaux@etu.imt-nord-europe.fr>
2022-03-10 16:31:05 +0100

    Modification pour affichage des images/fichiers

--- TP2/Ex7/en/cv.php
+++ TP2/Ex7/en/cv.php
@@ -1,3 +1,3 @@
 
 <h2> Resume M1 IMT Nord-Europe</h2>
-<embed src = "IDAW/TP2/Ex7/en/CV Léa Grumiaux Stage M1" width=800 height=500 type='application/pdf'/>
+<embed src ="CV Internship M1 Léa Grumiaux.pdf" width=800 height=500 type='application/pdf'/>
