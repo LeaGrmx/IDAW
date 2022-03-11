fd3bd5a5f6efa3cf4c783787be017c27d4b4ca61
LeaGrmx <lea.grumiaux@etu.imt-nord-europe.fr>
2022-03-11 12:17:37 +0100

    fin partie 1.3 style css

--- TP3/index.php
+++ TP3/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+    <div class=element-flexible taille25>
     <h3> Bienvenue sur mon site de test! </h3>
     <form id="style_form" action="index.php" method="GET">
         <select name="css">
@@ -9,6 +10,7 @@
         <input type="submit" value="Appliquer" />
     </form>
     <p> Ceci est mon site de test, il est en cours de construction... </p>
+
 
     <?php
         if(isset($_GET['css'])){
@@ -24,5 +26,14 @@
         $id_css = $_COOKIE['css'];
         echo "<br> L'identifiant css contenu dans le cookie est : ".$id_css;
     ?>
+    </div>
+    <head>
+        <title>Site de test</title>
+            <?php 
+                echo "<link rel=\"stylesheet\" href=\"".$id_css.".css\" type=\"text/css\"
+                media=\"screen\" title=\"default\" charset=\"utf-8\"/>";
+            ?>
+            
+    </head>
 
 </html>
