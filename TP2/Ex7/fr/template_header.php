45faca8e50cb7fcdc076984f3320ea3fcfb99974
LeaGrmx <lea.grumiaux@etu.imt-nord-europe.fr>
2022-03-11 10:22:36 +0100

    Suppression fichiers inutiles

--- TP2/Ex7/fr/template_header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Léa Grumiaux</title>
-            <link rel="stylesheet" href="cours.css" type="text/css"
-                media="screen" title="default" charset="utf-8"/>
-    </head>
-    <body>
--- TP2/Ex7/template_header.php
+++ TP2/Ex7/template_header.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Léa Grumiaux</title>
-            <link rel="stylesheet" href="fr\cours.css" type="text/css"
+            <link rel="stylesheet" href="cours.css" type="text/css"
                 media="screen" title="default" charset="utf-8"/>
     </head>
     <body>
