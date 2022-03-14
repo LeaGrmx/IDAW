199123efcc090a48523b0f1562c2655716b6eb2e
LeaGrmx <lea.grumiaux@etu.imt-nord-europe.fr>
2022-03-14 16:08:09 +0100

    Partie 1 Ex 2

--- TP3/Ex2/testConnexion.php
+++ TP3/Ex2/testConnexion.php
@@ -3,18 +3,41 @@
     <body>
         <h1>Bases de données MySQL</h1>  
         <?php
-            $servername = 'eden.imt-nord-europe.fr/';
-            $username = 'lea.grumiaux';
-            $password = 'LeaGrmx59**';
+            $servername = 'localhost';
+            $username = 'root';
+            $password = 'root';
             
             //On établit la connexion
-            $conn = mysqli_connect($servername, $username, $password);
+            $mysqli = mysqli_connect($servername, $username, $password, 'IDAW');
             
             //On vérifie la connexion
-            if(!$conn){
+            if(!$mysqli){
                 die('Erreur : ' .mysqli_connect_error());
             }
-            echo 'Connexion réussie';
+            echo 'Connexion réussie <br>';
+            
+            $result = mysqli_query($mysqli, "SELECT DATABASE()");
+            $row = mysqli_fetch_row($result);
+            printf("Default database is %s.\n", $row[0]);
+
+            $requete = mysqli_query($mysqli, "SELECT * FROM User");
+            echo '<br>';
+            print_r($requete);
+            echo'<br><br>';
+            echo "<table>
+                    <th>ID</th>
+                    <th>LOGIN</th>
+                    <th>PASSWORD</th>
+                    <th>PSEUDO</th>";
+            while ($row = $requete->fetch_assoc()) {
+                echo "<tr><td>".$row['ID']."</td>
+                        <td>".$row['LOGIN']."</td>
+                        <td>".$row['PASSWORD']."</td>
+                        <td>".$row['PSEUDO']."</td></tr>";
+            }
+            echo "</table>";
+            print_r($row['login']);
+
         ?>
     </body>
 </html>
