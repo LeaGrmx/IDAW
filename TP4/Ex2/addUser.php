fd10dc4749627f9a78a9235fd79ee122a1cfd2e6
LeaGrmx <lea.grumiaux@etu.imt-nord-europe.fr>
2022-03-22 22:53:01 +0100

    Fin requête Add + création fichier delete

--- TP4/Ex2/addUser.php
+++ TP4/Ex2/addUser.php
@@ -1,58 +1,23 @@
 <?php
-    header("Content-type: application/json");
-    $personnes = 
-    array(
-            array
-            (
-            "name" => "Jack",
-            "title" => "Magager",
-            "salary" => "$60,000",
-            ),
- 
-            array
-            (
-            "name" => "Joe",
-            "title" => "Developer",
-            "salary" => "$50,000",
-            ),
- 
-            array
-            (
-            "name" => "Susan",
-            "title" => "Marketer",
-            "salary" => "$50,000",
-            )
-        );
- 
-    echo json_encode($personnes);
-?>
-
-   
-<?php
-    // on simule une base de données
-    $servername = 'localhost';
-    $username = 'root';
-    $password = 'root';
     
-    //On établit la connexion
-    $mysqli = mysqli_connect($servername, $username, $password, 'IDAW');
+    require_once("database.php");
     
-    //On vérifie la connexion
-    if(!$mysqli){
-        die('Erreur : ' .mysqli_connect_error());
-    }
+    console.log($_POST['nom']);
+    if(isset($_POST['nom'])){
+        $nom = $_POST['nom'];
+        $prenom = $_POST['prenom'];
+        $date = $_POST['date'];
+        $cours = $_POST['cours'];
+        $remarques = $_POST['remarques'];
 
-    if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['birth']) && isset($_POST['cours']) && isset($_POST['remarques'])){
-        $nom = $connection->real_escape_string($_POST['nom']);
-        $prenom = $connection->real_escape_string($_POST['prenom']);
-        $birth = $connection->real_escape_string($_POST['birth']);
-        $cours = $connection->real_escape_string($_POST['cours']);
-        $remarques = $connection->real_escape_string($_POST['remarques']);
-
-        $query = "INSERT INTO UTILISATEUR (Nom, Prénom, Date_de_naissance, Aime_le_cours, Remarques) VALUES ('$nom', '$prenom', '$birth', $cours, '$remarques')";
-        $result = $connection->query($query);
+        $query = "INSERT INTO UTILISATEUR (Nom,Prénom,Date_de_naissance,Aime_le_cours_web,Remarques) VALUES ('$nom', '$prenom', '$date', '$cours', '$remarques')";
+        $result = mysqli_query($mysqli, $query);
 
         if($result)
-            echo $connection->insert_id;
+            echo json_encode($result);
+        
+        else{
+            echo 'Erreur';
+        }
     }
 ?>
