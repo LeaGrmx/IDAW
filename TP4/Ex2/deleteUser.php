66aa05dcd9847be3f2e48bafbde0bdbf00d7dbcd
LeaGrmx <lea.grumiaux@etu.imt-nord-europe.fr>
2022-03-23 11:59:28 +0100

    modif requête delete

--- TP4/Ex2/deleteUser.php
+++ TP4/Ex2/deleteUser.php
@@ -4,13 +4,13 @@
 
     if(isset($_POST['nom'])){
         $personne = $_POST['personne'];
-        $nom = personne[0];
-        $prenom = personne[1];
-        $date = personne[2];
-        $cours = personne[3];
-        $remarques = personne[4];
+        $nom = $personne['nom'];
+        $prenom = $personne['prenom'];
+        $date = $personne['date'];
+        $cours = $personne['cours'];
+        $remarques = $personne['remarques'];
 
-        $query = "DELETE FROM UTILISATEUR WHERE (Nom='$nom' AND Prénom='$prenom' AND Date_de_naissance='$date' AND Aime_le_cours_web ='$cours' AND Remarques='$remarques')";
+        $query = "DELETE FROM UTILISATEUR WHERE (Nom='$nom' AND Prénom='$prenom' AND Date_de_naissance='$date')";
         $result = mysqli_query($mysqli, $query);
 
         if($result)
